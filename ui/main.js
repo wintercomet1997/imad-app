440782bba57b98e38b9e1f6c003ddc0b10ae8b18
wintercomet1997 <36768167+wintercomet1997@users.noreply.github.com>
2018-02-24 16:28:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ var element=document.getElementById("main-text");
  marginLeft=0;
  function moveRight()
  {
-     marginLeft=marginLeft+1;
+     marginLeft=marginLeft+100;
      img.style.marginLeft=marginLeft+"px";
  }
  img.onclick=function(){
