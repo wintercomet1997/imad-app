f13c3f154933ee842301d905953a3dad5eb10f37
wintercomet1997 <36768167+wintercomet1997@users.noreply.github.com>
2018-02-24 12:48:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/article2',function(req,res)
 });
 app.get('/article3',function(req,res)
 {
-   res.send("article 3 request will be served here") ;
+    res.sendFile(path.join(__dirname, 'ui', 'article3.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
