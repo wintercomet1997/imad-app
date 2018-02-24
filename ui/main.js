7c55a126bb09f0f1195ba651a21379680389f951
wintercomet1997 <36768167+wintercomet1997@users.noreply.github.com>
2018-02-24 16:17:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 //change text
 var element=document.getElementById("main-text");
  element.innerHTML="New Value";
+ //move the image
+ var img=document.getElementById("img");
+ img.onclick=function(){
+     img.style.marginleft="100px";
+     
+ }
